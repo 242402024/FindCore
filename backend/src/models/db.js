9d621d63b58176543c408d03156060cf4c177e50
242402024 <pv24240@alunos.estgv.ipv.pt>
2024-07-12 22:35:07 +0100

    Delete backend directory

--- backend/src/models/db.js
+++
@@ -1,8 +0,0 @@
-var Sequelize = require("sequelize");
-const sequelize = new Sequelize("findcoreproject", "postgres", "Minecraftnerf_1", {
-  host: "localhost",
-  port: "5432",
-  dialect: "postgres",
-});
-
-module.exports = sequelize;
